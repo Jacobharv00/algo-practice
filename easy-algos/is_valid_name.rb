f261c774d1275d340fe1e8bf5761408d0dbcbf71
jacob <jacobharv00@gmail.com>
2022-01-27 11:11:20 -0700

    completed is valid name problem

---
+++ easy-algos/is_valid_name.rb
@@ -0,0 +1,32 @@
+# A name is valid is if satisfies all of the following:
+# - contains at least a first name and last name, separated by spaces
+# - each part of the name should be capitalized
+
+def is_valid_name(str)
+  names_arr = str.split(" ")
+  
+  if names_arr.length < 2
+    return false
+  end
+
+  names_arr.each do |name|
+    if !is_capitalized(name)
+      return false
+    end
+  end
+
+  return true
+end
+
+def is_capitalized(word)
+  if word[0] == word[0].upcase && word[1..-1] == word[1..-1].downcase
+    return true
+  else
+    return false
+  end
+end
+
+puts is_valid_name("Kush Patel")       # => true
+puts is_valid_name("Daniel")           # => false
+puts is_valid_name("Robert Downey Jr") # => true
+puts is_valid_name("ROBERT DOWNEY JR") # => false
