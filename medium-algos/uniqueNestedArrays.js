e4714a2457066175db0ea38fde14163dfdc87753
jacob <jacobharv00@gmail.com>
2022-01-28 12:01:40 -0700

    unique nested arrays problem

---
+++ medium-algos/uniqueNestedArrays.js
@@ -0,0 +1,12 @@
+function uniteUnique ( ...arr ) {
+  return [ ...new Set( arr.flat() ) ]
+}
+
+uniteUnique( [ 1, 3, 2 ], [ 5, 2, 1, 4 ], [ 2, 1 ] ) // => [1, 3, 2, 5, 4]
+uniteUnique( [ 1, 2, 3 ], [ 5, 2, 1 ] ) // => [1, 2, 3, 5]
+uniteUnique( [ 1, 2, 3 ], [ 5, 2, 1, 4 ], [ 2, 1 ], [ 6, 7, 8 ] ) // => [1, 2, 3, 5, 4, 6, 7, 8]
+
+// Instructions
+// Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+// In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+// The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
